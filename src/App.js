82513a3d830c0f94026bc5a16d8edab978c2fc9a
Mario Esteban Mateo <memateo@gmail.com>
2023-02-28 15:43:57 -0300

    starting project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Certifications from "./components/Certifications";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Certifications />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            My Portfolio
+          </a>
+          <a
+            href="https://my-porfolio-delta.vercel.app/src/assets/MarioEstebanMateoResume.pdf"
+            download="Mario's Resume"
+            target="_blank"
+          >
+            <button type="button" class="btn btn-success">
+              Download Resume
+            </button>
+          </a>
+          <div>
+            <div className="social-buttons ms-2">
+              <div className="button1">
+                <a href="https://github.com/MarioEstebanMateo" target="_blank">
+                  <i className="fa-brands fa-github text-black"></i>
+                </a>
+              </div>
+              <div className="button2">
+                <a
+                  href="https://www.linkedin.com/in/marioestebanmateo"
+                  target="_blank"
+                >
+                  <i className="fa-brands fa-linkedin text-black"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
