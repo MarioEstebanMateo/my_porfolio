27986a78ef581ce8bd4a294d79ec7f61a36f145f
Mario Esteban Mateo <memateo@gmail.com>
2024-06-02 16:51:55 -0300

    finished portfolio 2.0

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,23 @@
 import Swal from "sweetalert2";
 import emailjs from "@emailjs/browser";
 import { useRef } from "react";
+import { useState } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
+
+import logoGmail from "../assets/icons/logoGmail.svg";
+
+import wandIcon from "../assets/icons/wand.svg";
 
 export const Contact = () => {
   const form = useRef();
+  const [isExploding, setIsExploding] = useState(false);
+
+  const props = {
+    force: 2.0,
+    duration: 10000,
+    particleCount: 500,
+    width: 1920,
+  };
 
   const resetForm = () => {
     form.current.reset();
@@ -30,8 +44,26 @@ export const Contact = () => {
       );
   };
 
+  const handleConfetti = () => {
+    setIsExploding(true);
+    setTimeout(() => {
+      setIsExploding(false);
+    }, 10000);
+  };
+
   return (
     <div className="m-5">
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-primary text-white p-2 rounded-md my-5 flex items-center justify-center font-opensans font-semibold text-lg hover:bg-black"
+          onClick={handleConfetti}
+        >
+          <img src={wandIcon} alt="Wand Icon" className="mx-2 h-6" />
+          Click Me!!
+          <img src={wandIcon} alt="Wand Icon" className="mx-2 h-6" />
+        </button>
+        {isExploding && <ConfettiExplosion {...props} />}
+      </div>
       <div>
         <h2 className="text-3xl font-bold text-center text-primary sm:text-4xl font-opensans">
           Contact
@@ -46,7 +78,7 @@ export const Contact = () => {
       <form
         ref={form}
         onSubmit={sendEmail}
-        className="flex flex-col items-center justify-center w-full mt-10"
+        className="flex flex-col items-center justify-center w-full mt-3"
       >
         <div className="flex flex-col items-center justify-center w-3/4 md:w-1/2">
           <label
@@ -57,7 +89,7 @@ export const Contact = () => {
           </label>
           <input
             type="text"
-            className="w-full p-2 mt-2 border border-primary rounded-md"
+            className="w-full p-2 mt-2 border-2 border-primary rounded-md"
             name="name"
             id="name"
             placeholder="Enter your name"
@@ -73,7 +105,7 @@ export const Contact = () => {
           </label>
           <input
             type="email"
-            className="w-full p-2 mt-2 border border-primary rounded-md"
+            className="w-full p-2 mt-2 border-2 border-primary rounded-md"
             name="email"
             id="email"
             placeholder="Enter your email"
@@ -88,7 +120,7 @@ export const Contact = () => {
             Message
           </label>
           <textarea
-            className="w-full p-2 mt-2 border border-primary rounded-md"
+            className="w-full p-2 mt-2 border-2 border-primary rounded-md"
             name="message"
             id="message"
             placeholder="Enter your message"
@@ -98,10 +130,32 @@ export const Contact = () => {
         <div className="flex flex-col items-center justify-center w-3/4 md:w-1/2 mt-5">
           <button
             type="submit"
-            className="w-full p-2 text-lg font-semibold text-white bg-primary rounded-md font-opensans"
+            className="font-opensans text-primary bg-transparent border-2 border-solid border-primary hover:bg-primary hover:text-white active:bg-primary font-bold uppercase text-lg px-2 sm:px-4 py-2 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 w-full"
           >
             Send
           </button>
+        </div>
+        <div className="flex justify-center items-center gap-3 mt-5">
+          <div>
+            <p className="text-lg font-semibold text-center text-primary font-opensans">
+              You can also contact me through my email:{" "}
+            </p>
+          </div>
+          <div className="hover:scale-125 ease-in-out duration-300">
+            <a
+              href="mailto:memateo@gmail.com
+"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={logoGmail} alt="logo Gmail" />
+            </a>
+          </div>
+        </div>
+        <div>
+          <p className="text-lg font-semibold text-center text-primary font-opensans">
+            I´ll be in touch with you as soon as possible!
+          </p>
         </div>
       </form>
     </div>
